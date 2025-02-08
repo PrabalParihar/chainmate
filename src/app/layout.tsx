23a12971d4c5b51254ae04ed306e93f0e826c461
PrabalParihar <prabalparihar95@gmail.com>
2025-02-08 22:08:28 +0530

    add global styles and custom font integration

---
+++ src/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import { PrivyClientProvider } from "@/Providers/PrivyProvider";
+import {Toaster} from "react-hot-toast"
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "ChainMate",
+  description: "Full form - Self-evolving Yottascale Network eXchange",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <PrivyClientProvider>{children}</PrivyClientProvider>
+        <Toaster/>
+      </body>
+    </html>
+  );
+}
